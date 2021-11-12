9fea1d1faeba0cd71b233548b6885e3ee40cc3b7
Alberto Garibay <garib112@mail.chapman.edu>
2021-11-12 11:55:21 -0800

    New Changes
    Added
    - GLTFLoader library
    - gsap library
    
    Messed around with 3d object functionality by adding in a light to the scene and making the doll rotate. Also adding in geometric shapes to help add to the scene.

--- squid-game-tut/js/main.js
+++ squid-game-tut/js/main.js
@@ -5,16 +5,51 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-/*
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-*/
+renderer.setClearColor( 0xb7c3f3, 1);
+const light = new THREE.AmbientLight( 0xffffff );
+scene.add( light );
+
+
+function createCube(size){
+    const geometry = new THREE.BoxGeometry(size.w, size.h, size.d);
+    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    const cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+}
 
 camera.position.z = 5;
+const loader = new THREE.GLTFLoader()
 
-const loader = new GLTFLoader();
+class Doll {
+    constructor(){
+        loader.load("/squid-game-tut/models/scene.gltf" , (gltf) => {
+            scene.add( gltf.scene );
+            gltf.scene.scale.set(.4,.4,.4);
+            gltf.scene.position.set(0,-1,0);
+            this.doll = gltf.scene;
+        }) 
+    }
+
+    lookBackward(){
+        //this.doll.rotation.y = -3.15;
+        gsap.to(this.doll.rotation, {y: -3.15, duration: .45})
+    }
+
+    lookForward(){
+        gsap.to(this.doll.rotation, {y: 0, duration: .45})
+    }
+}
+
+function createTrack(){
+    createCube({w: .2, h: 1.5 , d:1});
+}
+
+createTrack();
+
+//let doll = new Doll();
+setTimeout(() => {
+    doll.lookBackward()
+}, 1000);
 
 function animate() {
 	renderer.render( scene, camera );
@@ -22,3 +57,11 @@ function animate() {
     requestAnimationFrame( animate );
 }
 animate();
+
+window.addEventListener( 'resize', onWindowResize, false);
+
+function onWindowResize(){
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight);
+}
