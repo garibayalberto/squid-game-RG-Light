e7fde7e7c04af2e3e7a20346465543c6462b60e4
Alberto Garibay <garib112@mail.chapman.edu>
2021-11-18 11:55:19 -0800

    Track + Game Logic
    - Added the track to the game
    - Added a timer
    - Added functionality to the player
    - Started adding game logic

--- squid-game-tut/js/main.js
+++ squid-game-tut/js/main.js
@@ -9,17 +9,30 @@ renderer.setClearColor( 0xb7c3f3, 1);
 const light = new THREE.AmbientLight( 0xffffff );
 scene.add( light );
 
+// global variables
+const start_position = 3;
+const end_position = -start_position;
+const text = document.querySelector(".text");
+const TIMIT_LIMIT = 10;
+let gameStat = "loading";
+let isLookingBackward = true;
 
-function createCube(size){
+function createCube(size , positionX, rotY = 0, color = 0xfbc851){
     const geometry = new THREE.BoxGeometry(size.w, size.h, size.d);
-    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    const material = new THREE.MeshBasicMaterial( { color: color } );
     const cube = new THREE.Mesh( geometry, material );
+    cube.position.x = positionX;
+    cube.rotation.y = rotY;
     scene.add( cube );
+    return cube;
 }
 
 camera.position.z = 5;
 const loader = new THREE.GLTFLoader()
 
+function delay(ms){
+    return new Promise (resolve => setTimeout(resolve, ms)); 
+}
 class Doll {
     constructor(){
         loader.load("/squid-game-tut/models/scene.gltf" , (gltf) => {
@@ -31,30 +44,100 @@ class Doll {
     }
 
     lookBackward(){
-        //this.doll.rotation.y = -3.15;
         gsap.to(this.doll.rotation, {y: -3.15, duration: .45})
+        setTimeout(() => isLookingBackward = true, 450);
     }
 
     lookForward(){
         gsap.to(this.doll.rotation, {y: 0, duration: .45})
+        setTimeout(() => isLookingBackward = false, 150);
+    }
+
+    async start(){
+        this.lookBackward();
+        await delay((Math.random() * 1000) + 1000);
+        this.lookForward();
+        await delay((Math.random() * 750) + 750);
+        this.start();
     }
 }
 
 function createTrack(){
-    createCube({w: .2, h: 1.5 , d:1});
+    createCube({w: start_position*2 +.2, h: 1.5 , d:1}, 0, 0,0xe5a716).position.z = -1;
+    createCube({w: .2, h: 1.5 , d:1}, start_position, -.35);
+    createCube({w: .2, h: 1.5 , d:1}, end_position, .35);
+    
 }
 
 createTrack();
 
-//let doll = new Doll();
-setTimeout(() => {
-    doll.lookBackward()
-}, 1000);
+class Player{
+    constructor(){
+        const geometry = new THREE.SphereGeometry( .3, 32, 16 );
+        const material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+        const sphere = new THREE.Mesh( geometry, material );
+        sphere.position.z = 1;
+        sphere.position.x = start_position;
+        scene.add( sphere );
+        this.player = sphere;
+        this.playerInfo = {
+            positionX: start_position,
+            velocity: 0,
+        }
+    }
+    run(){
+        this.playerInfo.velocity = .03;
+    }
+
+    stop(){
+        gsap.to(this.playerInfo, {velocity:0, duration: .1});
+    }
+
+    check(){
+        if(this.playerInfo.velocity > 0 && !isLookingBackward){
+            alert("You Lose!");
+        }
+        if(this.playerInfo.positionX < end_position){
+            alert("You win!");
+        }
+    } 
+
+    update(){
+        this.check();
+        this.playerInfo.positionX -= this.playerInfo.velocity;
+        this.player.position.x = this.playerInfo.positionX;
+    }   
+}
+
+const player = new Player();
+let doll = new Doll();
+
+async function init() {
+    await delay(1000);
+    text.innerText = "Starting in 3";
+    await delay(1000);
+    text.innerText = "Starting in 2";
+    await delay(1000);
+    text.innerText = "Starting in 1";
+    await delay(1000);
+    text.innerText = "Go!!!!!";
+    startGame();
+}
+
+function startGame() {
+    gameStat = "started";
+    let progressBar = createCube({w: 5, h: .1, d: 1}, 0);
+    progressBar.position.y = 3.35;
+    gsap.to(progressBar.scale, {x: 0, duration: TIMIT_LIMIT, ease: "none"});
+    doll.start();
+}
+
+init();
 
 function animate() {
 	renderer.render( scene, camera );
-
     requestAnimationFrame( animate );
+    player.update();
 }
 animate();
 
@@ -65,3 +148,16 @@ function onWindowResize(){
     camera.updateProjectionMatrix();
     renderer.setSize( window.innerWidth, window.innerHeight);
 }
+
+window.addEventListener('keydown', (e) => {
+    if(gameStat != "started") return
+    if(e.key == "ArrowUp"){
+        player.run();
+    }
+})
+
+window.addEventListener('keyup', (e) => {
+    if(e.key == "ArrowUp"){
+        player.stop();
+    }
+})
